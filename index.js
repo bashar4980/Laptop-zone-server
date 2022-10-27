751cbce2de18d1f80942029e958f5ed8ae42d2f5
bashar4980 <sagor.ahmed3669@gmail.com>
2022-10-27 15:34:11 +0600

    Cors install is done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+app.use(cors())
 const course_Data = require('./Data/data.json')
 
 app.get("/", (req, res)=>{
